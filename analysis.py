82fcb411a1e3eb016f2b0945fb30dbc4d0dd78df
DEVitor <devitormoreira@gmail.com>
2025-02-27 12:34:31 -0600

    Tratando dados

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-dados = pd.read_csv('./dates.csv', sep=';')
+dados = pd.read_csv('OK_Anexo_Arquivo_Dados_Projeto.csv', sep=';')
 
 dados.head(10)
 type(dados)
@@ -8,3 +8,9 @@ dados.shape
 dados.columns
 dados.info()
 dados.isnull().sum()
+
+dados.query('precip < 0').index
+registros_a_remover = dados.query('precip < 0').index
+dados.drop(registros_a_remover, axis=0, inplace=True)
+dados.query('precip < 0')
+dados.head(10)
